d00d02e2f3c1b359e80a02e79054ad85427ebeb1
Ansurko <Anss.inst@gmail.com>
2024-03-28 14:09:58 +0400

    Add migrations

--- core/models.py
+++ core/models.py
@@ -6,7 +6,7 @@ class Customers(models.Model):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
-    role_id = models.IntegerField(max_length=5)
+    role_id = models.IntegerField()
 
 
 class Status(models.Model):
@@ -15,8 +15,8 @@ class Status(models.Model):
 
 class Orders(models.Model):
     date = models.DateField(auto_now_add=True)
-    customer_id = models.IntegerField(max_length=5)
-    status_id = models.IntegerField(max_length=5)
+    customer_id = models.IntegerField()
+    status_id = models.IntegerField()
 
 
 class Type(models.Model):
@@ -28,10 +28,10 @@ class Gift(models.Model):
     price = models.FloatField(max_length=3)
     description = models.TextField(max_length=255)
     image = models.ImageField(verbose_name=None, height_field=None, width_field=None, max_length=100)
-    type_id = models.IntegerField(max_length=100)
+    type_id = models.IntegerField()
 
 
 class Sale(models.Model):
-    gift_id = models.IntegerField(max_length=100)
-    order_id = models.IntegerField(max_length=100)
-    amount = models.IntegerField(max_length=100)
+    gift_id = models.IntegerField()
+    order_id = models.IntegerField()
+    amount = models.IntegerField()
